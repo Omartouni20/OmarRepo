b86c7f2a50c3d892146b1ae97b7c85e3b394760d
Omartouni20 <saidtonyz26@gmail.com>
2024-08-17 20:39:41 +0300

    client v.2

---
+++ Client.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+using namespace std;
+class Client
+{
+  public:
+    void sayHello()
+    {
+        cout << "Welcome To AARO" << endl;
+        cout << "Hello my Client" << endl;
+    }
+};
